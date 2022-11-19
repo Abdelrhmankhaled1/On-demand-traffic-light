e857d865a143d78fa9e6c7c0b24378ebb9b19e86
Abdelrhmankhaled1 <114113596+Abdelrhmankhaled1@users.noreply.github.com>
2022-11-19 04:53:40 +0200

    Delete On-demand traffic light/Debug directory

--- On-demand traffic light/Debug/ECUAL/Buttons/Buttons.d
+++
@@ -1,7 +0,0 @@
-ECUAL/Buttons/Buttons.d ECUAL/Buttons/Buttons.o: \
- ../ECUAL/Buttons/Buttons.c ../ECUAL/Buttons/buttons.h \
- ../ECUAL/Buttons/../../MCAL/DIO/DIO.h
-
-../ECUAL/Buttons/buttons.h:
-
-../ECUAL/Buttons/../../MCAL/DIO/DIO.h:
--- On-demand traffic light/Debug/MCAL/Timers/Timers.d
+++
@@ -1,6 +0,0 @@
-MCAL/Timers/Timers.d MCAL/Timers/Timers.o: ../MCAL/Timers/Timers.c \
- ../MCAL/Timers/Timers.h ../MCAL/Timers/../DIO/DIO.h
-
-../MCAL/Timers/Timers.h:
-
-../MCAL/Timers/../DIO/DIO.h:
