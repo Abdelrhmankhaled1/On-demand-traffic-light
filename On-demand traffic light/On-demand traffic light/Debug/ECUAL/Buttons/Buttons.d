b941fc07d94064d4ea3f7bf495b7294233144ab3
Abdelrhmankhaled1 <114113596+Abdelrhmankhaled1@users.noreply.github.com>
2022-11-24 23:54:04 +0200

    Delete On-demand traffic light/On-demand traffic light directory

--- On-demand traffic light/On-demand traffic light/Debug/ECUAL/Buttons/Buttons.d
+++
@@ -1,7 +0,0 @@
-ECUAL/Buttons/Buttons.d ECUAL/Buttons/Buttons.o: \
- ../ECUAL/Buttons/Buttons.c ../ECUAL/Buttons/Buttons.h \
- ../ECUAL/Buttons/../../MCAL/DIO/DIO.h
-
-../ECUAL/Buttons/Buttons.h:
-
-../ECUAL/Buttons/../../MCAL/DIO/DIO.h:
